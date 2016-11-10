d7f0745b829a7903817a556123805b23450c633d
FrankYao <570468837@qq.com>
2016-11-10 12:02:30 +0800

    添加jquery

--- WebContent/pages/js/addRequirement.js
+++ WebContent/pages/js/addRequirement.js
@@ -1,11 +1,16 @@
 function initData(){
 	var iters = [
-		{"content": "迭代1"},
-		{"content": "迭代2"},
-		{"content": "迭代3"}
+		{"id": "1", "name": "迭代1"},
+		{"id": "2", "name": "迭代2"},
+		{"id": "3", "name": "迭代3"}
 	];
 	var select_iter = document.getElementById('select_iter');
 	for(var i=0; i<iters.length;i++){
-		select_iter.add(new Option(iters[i].content,i)); 
+		select_iter.add(new Option(iters[i].name,iters[i].id)); 
 	}
+	
+}
+
+function add(){
+	
 }
